bb3b18f7c1aeed27e40589daed3be60ba3a4e594
andrespaida <jordanpaida.bj@hotmail.com>
2024-11-12 21:59:04 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// index.php
+
+// Display a "Hello World" message
+echo "<h1>Welcome to My Simple PHP Website!</h1>";
+
+// Display the current date and time
+echo "<p>Current Date and Time: " . date('Y-m-d H:i:s') . "</p>";
+?>
